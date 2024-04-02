11cd6299c411ba37ed788148160526dbcb8bb73a
abdullah Al Omar <abdullahalomar048@gmail.com>
2024-04-03 01:24:40 +0600

    add create specialty in redux

---
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/components/SpecialtyModal.tsx
@@ -0,0 +1,59 @@
+import PHFileUploader from "@/components/Forms/PHFileUploader";
+import PHForm from "@/components/Forms/PHForm";
+import PHInput from "@/components/Forms/PHInput";
+import PHModal from "@/components/shared/PHModal/PHModal";
+import { useCreateSpecialtyMutation } from "@/redux/api/specialtiesApi";
+import { modifyPayload } from "@/utils/modifyPayload";
+import { Button, Grid } from "@mui/material";
+import { FieldValues } from "react-hook-form";
+import { toast } from "sonner";
+
+type TProps = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const SpecialtyModal = ({ open, setOpen }: TProps) => {
+  const [createSpecialty] = useCreateSpecialtyMutation();
+
+  const handleFormSubmit = async (values: FieldValues) => {
+    const data = modifyPayload(values);
+    try {
+      const res = await createSpecialty(data).unwrap();
+      console.log(res);
+
+      if (res?.id) {
+        toast.success("Specialty created successfully");
+        setOpen(false);
+      }
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  };
+  return (
+    <div>
+      <PHModal open={open} setOpen={setOpen} title="Create A New Specialist">
+        <PHForm onSubmit={handleFormSubmit}>
+          <Grid container spacing={2}>
+            <Grid item md={6}>
+              <PHInput
+                name="title"
+                label="title"
+                size={"small"}
+                fullWidth={false}
+              />
+            </Grid>
+            <Grid item md={6}>
+              <PHFileUploader name="file" label="Upload File"></PHFileUploader>
+            </Grid>
+          </Grid>
+          <Button sx={{ mt: 1 }} type="submit">
+            Create
+          </Button>
+        </PHForm>
+      </PHModal>
+    </div>
+  );
+};
+
+export default SpecialtyModal;
--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+import { Box, Button, Stack, TextField } from "@mui/material";
+import { useState } from "react";
+import SpecialtyModal from "./components/SpecialtyModal";
+
 const SpecialtiesPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   return (
-    <div>
-      <h1>Specialties Page </h1>
-    </div>
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Button onClick={() => setIsModalOpen(true)}>Create Specialty</Button>
+        <SpecialtyModal open={isModalOpen} setOpen={setIsModalOpen} />
+        <TextField size="small" placeholder="Search Specialist" />
+      </Stack>
+    </Box>
   );
 };
 
