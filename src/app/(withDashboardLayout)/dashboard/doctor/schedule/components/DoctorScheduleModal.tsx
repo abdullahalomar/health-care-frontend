70735f1d4e3126351ca7fe1a6e133221579d171a
abdullah Al Omar <abdullahalomar048@gmail.com>
2024-04-29 22:48:58 +0600

    doctor schedule added

---
+++ src/app/(withDashboardLayout)/dashboard/doctor/schedule/components/DoctorScheduleModal.tsx
@@ -0,0 +1,58 @@
+import PHModal from "@/components/shared/PHModal/PHModal";
+
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { useState } from "react";
+import dayjs, { Dayjs } from "dayjs";
+import { useGetAllSchedulesQuery } from "@/redux/api/scheduleApi";
+import MultipleSelectFieldChip from "./multipleSelectFieldChip";
+
+type TProps = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const DoctorScheduleModal = ({ open, setOpen }: TProps) => {
+  const [selectedDate, setSelectedDate] = useState(
+    dayjs(new Date()).toISOString()
+  );
+
+  const [selectedScheduleIds, setSelectedScheduleIds] = useState();
+
+  // console.log(selectedDate);
+  const query: Record<string, any> = {};
+  if (!!selectedDate) {
+    query["startDate"] = dayjs(selectedDate)
+      .hour(0)
+      .minute(0)
+      .millisecond(0)
+      .toISOString();
+    query["endDate"] = dayjs(selectedDate)
+      .hour(23)
+      .minute(59)
+      .millisecond(999)
+      .toISOString();
+  }
+
+  const { data, isLoading } = useGetAllSchedulesQuery(query);
+  const schedule = data?.schedules;
+  // console.log(schedule);
+
+  return (
+    <PHModal open={open} setOpen={setOpen} title="Add Doctor Schedule">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          label="Controlled picker"
+          value={dayjs(selectedDate)}
+          onChange={(newValue) =>
+            setSelectedDate(dayjs(newValue).toISOString())
+          }
+        />
+      </LocalizationProvider>
+      <MultipleSelectFieldChip schedules={schedule} />
+    </PHModal>
+  );
+};
+
+export default DoctorScheduleModal;
---
+++ src/app/(withDashboardLayout)/dashboard/doctor/schedule/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Box, Button } from "@mui/material";
+import { useState } from "react";
+import DoctorScheduleModal from "./components/DoctorScheduleModal";
+
+const DoctorSchedulePage = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  return (
+    <Box>
+      <Button onClick={() => setIsModalOpen(true)}>Add Doctor Schedule</Button>
+      <DoctorScheduleModal open={isModalOpen} setOpen={setIsModalOpen} />
+    </Box>
+  );
+};
+
+export default DoctorSchedulePage;
